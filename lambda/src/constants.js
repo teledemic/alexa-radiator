ed908aea5a797ea72c38af42a0bbb6f79a78ea2d
Carl Irving <carl@teledemic.com>
2017-12-22 14:56:43 -0500

    Set skill id

--- lambda/src/constants.js
+++ lambda/src/constants.js
@@ -3,7 +3,7 @@
 module.exports = Object.freeze({
     
     //App-ID. TODO: set to your own Skill App ID from the developer portal.
-    //appId : 'amzn1.ask.skill.123',
+    appId : 'amzn1.ask.skill.4bb80d3d-53b7-4fe0-adf2-c7fc86dec7be',
 
     // when true, the skill logs additional detail, including the full request received from Alexa
     debug : false

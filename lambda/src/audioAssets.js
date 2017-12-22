57648ebb9ad9bef1db5683e5e037c7f3e58dee00
Carl Irving <carl@teledemic.com>
2017-12-22 14:48:58 -0500

    Initial commit

---
+++ lambda/src/audioAssets.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var audioData = {
+    title: 'The Radiator',
+    subtitle: '105.9 WOMM-LP',
+    cardContent: "Visit The Radiator at https://www.bigheavyworld.com/105-the-radiator/",
+    url: 'http://stream.theradiator.org/womm-lp_fm_256',
+    image: {
+        largeImageUrl: 'https://s3-eu-west-1.amazonaws.com/alexa.maxi80.com/assets/alexa-artwork-1200.png',
+        smallImageUrl: 'https://s3-eu-west-1.amazonaws.com/alexa.maxi80.com/assets/alexa-artwork-720.png'
+    }
+};
+
+module.exports = audioData;

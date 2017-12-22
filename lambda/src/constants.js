548c919a8267d97a59620d5451c91b105f1ac84d
Carl Irving <carl@teledemic.com>
2017-12-22 15:05:11 -0500

    New appid for real acct

--- lambda/src/constants.js
+++ lambda/src/constants.js
@@ -3,7 +3,7 @@
 module.exports = Object.freeze({
     
     //App-ID. TODO: set to your own Skill App ID from the developer portal.
-    appId : 'amzn1.ask.skill.4bb80d3d-53b7-4fe0-adf2-c7fc86dec7be',
+    appId : 'amzn1.ask.skill.75f0f5df-1c05-48a2-bf58-bb48042dcdf2',
 
     // when true, the skill logs additional detail, including the full request received from Alexa
     debug : false
